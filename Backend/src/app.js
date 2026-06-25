acdc9d47d2026904e27ab8013300c03a3016ead6
Kshitij Himanshu <kshitij7320@gmail.com>
2026-06-25 13:30:50 +0530

    added some error fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,14 +4,24 @@ import authRouter from "./routes/auth.routes.js";
 import chatRouter from "./routes/chat.routes.js";
 import morgan from "morgan";
 import cors from "cors";
+// import path from "path"
+// import { fileURLToPath } from "url";
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
 
 const app = express();
 
+
 // Middleware
 app.use(express.json());
+// app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
+// app.use("*name", (req, res)=>{
+//     res.sendFile(path.join(__dirname, "../public/index.html"));
+// })
 app.use(cors({
     origin: "http://localhost:5173",
     credentials: true,
