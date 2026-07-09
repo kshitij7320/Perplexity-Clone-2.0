398e65256b2a1a7a198d730220d57edb5f1fe716
Kshitij Himanshu <kshitij7320@gmail.com>
2026-07-09 10:54:44 +0530

    error fix

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -33,7 +33,7 @@ export async function register(req, res) {
                 <p>Hi ${username},</p>
                 <p>Thank you for registering at <strong>Perplexity</strong>. We're excited to have you on board!</p>
                 <p>Please verify your email address by clicking the link below:</p>
-                <a href="http://localhost:3000/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
+                <a href="https://perplexity-clone-2-0.onrender.com/api/auth/verify-email?token=${emailVerificationToken}">Verify Email</a>
                 <p>If you did not create an account, please ignore this email.</p>
                 <p>Best regards,<br>The Perplexity Team</p>
         `
@@ -154,7 +154,7 @@ export async function verifyEmail(req, res) {
         const html =`
         <h1>Email Verified Successfully!</h1>
         <p>Your email has been verified. You can now log in to your account.</p>
-        <a href="http://localhost:3000/login">Go to Login</a>`
+        <a href="https://perplexity-clone-2-0.onrender.com/login">Go to Login</a>`
 
         return res.send(html);
     } catch (err) {
